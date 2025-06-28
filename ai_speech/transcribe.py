4160cb3ac4e664d1b6857630b61dc4f157553b96
0AA01A0F1 <68499986+loneicewolf@users.noreply.github.com>
2025-06-28 06:58:31 -0400

    Update transcribe.py

--- ai_speech/transcribe.py
+++ ai_speech/transcribe.py
@@ -6,7 +6,31 @@ from typing import Dict, Any
 import whisper
 
 
-MODEL_NAME = "small"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# -------------------------------------- #
+## inside transcribe.py
+# MODEL_NAME = "small"
+# MODEL_NAME = "medium" Not tested yet
+MODEL_NAME="medium.en" # ENGLISH ONLY
+# -------------------------------------- #
+
+# copy from the Whisper DOCS
+
+## Available models and languages
+# 
+# There are six model sizes, four with English-only versions, offering speed and accuracy tradeoffs.
+# Below are the names of the available models and their approximate memory requirements and inference speed relative to the large model.
+# The relative speeds below are measured by transcribing English speech on a A100, and the real-world speed may vary significantly depending on many factors including the language, the speaking speed, and the available hardware.
+# 
+# |  Size  | Parameters | English-only model | Multilingual model | Required VRAM | Relative speed |
+# |:------:|:----------:|:------------------:|:------------------:|:-------------:|:--------------:|
+# |  tiny  |    39 M    |     `tiny.en`      |       `tiny`       |     ~1 GB     |      ~10x      |
+# |  base  |    74 M    |     `base.en`      |       `base`       |     ~1 GB     |      ~7x       |
+# | small  |   244 M    |     `small.en`     |      `small`       |     ~2 GB     |      ~4x       |
+# | medium |   769 M    |    `medium.en`     |      `medium`      |     ~5 GB     |      ~2x       |
+# | large  |   1550 M   |        N/A         |      `large`       |    ~10 GB     |       1x       |
+# | turbo  |   809 M    |        N/A         |      `turbo`       |     ~6 GB     |      ~8x       |
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 
 def transcribe_audio(audio_path: str) -> Dict[str, Any]:
