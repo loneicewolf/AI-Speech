602d2257aa2f7653654e5ae754bffa4d3e306f0d
0AA01A0F1 <68499986+loneicewolf@users.noreply.github.com>
2025-06-28 13:43:49 -0400

    Revert "Improve documentation and add action utilities"

--- ai_speech/__init__.py
+++ ai_speech/__init__.py
@@ -4,12 +4,10 @@ from .recorder import record_audio
 from .transcribe import transcribe_audio
 from .analyzer import analyze_disfluencies
 from .tips import generate_tips
-from .actions import run_action_mode
 
 __all__ = [
     "record_audio",
     "transcribe_audio",
     "analyze_disfluencies",
     "generate_tips",
-    "run_action_mode",
 ]
